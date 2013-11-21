9ed2573ef1112d61f831ba162db45dc743ce617a
Rodrigo Rosa <rodrigocrosa@gmail.com>
2013-11-21 19:11:12 -0200

    Criado projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiaDeTreinamento::Application.config.secret_key_base = '812588925b0a723ec9baf2eb5e1cdc4e9a076644fa713517ba5bcd7800965a59e0df8c2e6948fe76a21acaa3cb270d7c5d48cfb3d22e8bec2f8081769e502738'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiaDeTreinamento::Application.config.session_store :cookie_store, key: '_dia-de-treinamento_session'
